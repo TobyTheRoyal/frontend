5a845751115c98a2f8d9d88e875332eb78d5a6d1
TobyTheRoyal <tobias.hamedl@student.tugraz.at>
2025-07-17 09:46:06 +0200

    deleted cypress

--- cypress/e2e/home.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Home Page', () => {
-  it('should load and show hero section', () => {
-    cy.visit('http://localhost:4200');
-    cy.contains('Trending Now').should('exist');
-  });
-});
