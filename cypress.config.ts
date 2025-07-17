9ad799e7724bd68fb78579fa04d284c21b650e43
TobyTheRoyal <tobias.hamedl@student.tugraz.at>
2025-07-17 11:58:08 +0200

    cypress is working now

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'http://127.0.0.1:4200',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.ts',
     env: {
-      apiUrl: 'http://localhost:3000'
+      apiUrl: 'http://127.0.0.1:3000'
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://127.0.0.1:3000',
   tmdbApiUrl: 'https://api.themoviedb.org/3',
   tmdbApiKey: '1b3d7c196e53b4ebab10bf60054ef369'
 };
